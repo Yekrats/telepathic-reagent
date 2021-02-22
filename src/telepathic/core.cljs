fb39e5c115c8278f6e1eb965720ae83645e472e8
Jason Dufair <jase@dufair.org>
2021-02-22 13:03:22 -0500

    Render action cards & initial color/shape cards. Delete transient files

--- src/telepathic/core.cljs
+++ src/telepathic/core.cljs
@@ -6,28 +6,64 @@
    [cljs.pprint :refer [pprint]]
    [telepathic.logic :refer [condition-cards colors shapes initiate-actions sls asset-name]]))
 
-(println "This text is printed from src/telepathic/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:color-player (condition-cards colors)
-   :shape-player (condition-cards shapes)
-   :board (sls)
-   :actions (initiate-actions)
-   }))
+(defonce app-state (atom
+                    {:color-player (condition-cards colors)
+                     :shape-player (condition-cards shapes)
+                     :board (sls)
+                     :actions (initiate-actions)}))
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
-(defn hello-world []
-  [:div
-   [:h1 (pprint @app-state)]
-   [:h3 "Edit this in src/telepathic/core.cljs and watch it change!"]
-   [:table (map (fn [row] [:tr (map (fn [card] [:td [:img {:src (str "/images/" (asset-name card ))
-                                                           :class "card-image"}]]) row)])
-                                    (partition 4 (:board @app-state)))]])
+(defn render-board
+  "Render the game board from app state."
+  []
+  [:table
+   [:tbody
+    (map-indexed (fn [row-index row]
+                   [:tr {:key row-index}
+                    (map-indexed (fn [col-index card]
+                                   [:td {:key col-index}
+                                    [:img {:src (str "/images/" (asset-name card))
+                                           :class "card-image"}]])
+                                 row)])
+                 (partition 4 (:board @app-state)))]])
+
+(defn render-actions
+  "Render the four current actions from state."
+  []
+  [:div {:id "action-cards"
+         :style
+         {:display "flex"
+          :flex-direction "column"}}
+   (map-indexed (fn [index action]
+                  [:img {:src (str "/images/" (name action) ".png")
+                         :class "card-image action-card"
+                         :key index}])
+                (-> @app-state :actions :available))])
+
+(defn render-color-player
+  "Render the condition cards for the color player from state."
+  []
+  (str (:color-player @app-state)))
+
+(defn render-shape-player
+  "Render the condition cards for the shape playre from state."
+  []
+  (str (:shape-player @app-state)))
+
+(defn render-game []
+  [:<>
+   [:div {:style {:display "flex"}}
+    (render-board)
+    (render-actions)]
+   [:div
+    (render-color-player)
+    (render-shape-player)]])
 
 (defn mount [el]
-  (rdom/render [hello-world] el))
+  (rdom/render [render-game] el))
 
 (defn mount-app-element []
   (when-let [el (get-app-element)]
@@ -43,4 +79,4 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
